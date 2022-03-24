ce4c156c58847be1f14f11f66243a56128030acf
chikhang <khangk1390@gmail.com>
2022-03-24 14:22:27 +0700

    hello

---
+++ TodoApp/Controllers/Cryptooly.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp.Controllers
+{
+    public class Cryptooly : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult swadex()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TodoApp/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// step 1
+const ipnElement = document.querySelector('#ipnPassword')
+const btnElement = document.querySelector('#btnPassword')
+
+// step 2
+btnElement.addEventListener('click', function () {
+    // step 3
+    const currentType = ipnElement.getAttribute('type')
+    // step 4
+    ipnElement.setAttribute(
+        'type',
+        currentType === 'password' ? 'text' : 'password'
+    )
+})
